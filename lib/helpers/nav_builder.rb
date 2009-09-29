a9d729c99d7fcea184ea513924d50a75c15249f8
Lachlan Hardy <lachlan@lachstock.com.au>
2009-09-29 22:42:11 +1000

    tidying up after first deploy

--- lib/helpers/nav_builder.rb
+++ lib/helpers/nav_builder.rb
@@ -1,7 +1,7 @@
 module Killerdumptrucks
   module Helpers
     def nav_builder
-      @nav_items = ["about", "browse", "random"]
+      @nav_items = ["about"] #["about", "browse", "random"]
       haml(:"_navigation", :layout => false)
     end
   end
