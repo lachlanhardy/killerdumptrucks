b6092c2ba8cfe6445459b96a8c2bde510e15bc06
Lachlan Hardy <lachlan@lachstock.com.au>
2011-12-30 15:31:16 +1100

    OMG Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = 'e11a5459b6ec30ae954da26ecf996e0a6c7f80050d357d43dac657db29dfb2c6e65d692f3cc466babac969bba39c3235954e71a44b957c0a632d43dbd6e975cb'
